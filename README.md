5245fa2a6918d08495ec66adce7542514007968b
unknown <jaeoong@hotmail.com>
2017-06-29 16:10:32 +0900

    rebuilding

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # inven-attendance
+
+## CONTENTS
+1. SETUP
+2. USAGE
+
+### 1. SETUP
+SET VALUES  
+```
+attendance.py:8  
+attendance.py:9  
+```
+### 2. USAGE
+```
+python attendance.py
+```
--- attendance.py
+++ attendance.py
@@ -1,67 +1,83 @@
-from bs4 import BeautifulSoup
-import cookielib, urllib2, urllib
+import cookielib
+import urllib2
+import urllib
+import json
 from time import strftime, localtime
-
-cj = cookielib.CookieJar()
-opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-urllib2.install_opener(opener)
+from bs4 import BeautifulSoup
 
 ID = 'ID'
-PASSWORD = 'PWD'
+PASSWORD = 'PASSWORD'
 URL = 'https://member.inven.co.kr'
 
-class Requestor:
-    cj = cookielib.CookieJar()
-    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-    urllib2.install_opener(opener)
-    # def __init__(self):
 
-    def get(self, url, params, isSet):
+class Request:
+    def __init__(self):
+        self.cj = cookielib.CookieJar()
+        self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cj))
+        urllib2.install_opener(self.opener)
+
+    def get_plain(self, url, params='', headers={}):
         print url
-        req = urllib2.Request(url, params)
-        if (isSet):
-            req.add_header('Accept', '*/*')
-            req.add_header('Accept-Encoding', 'gzip, deflate')
-            req.add_header('Accept-Language', 'ko-KR,ko;q=0.8,en-US;q=0.6,en;q=0.4')
-            req.add_header('Cache-Control', 'no-cache')
-            req.add_header('Connection', 'keep-alive')
-            req.add_header('Content-Length', '17')
-            req.add_header('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8')
-            req.add_header('Host', 'imart.inven.co.kr')
-            req.add_header('Origin', 'http://imart.inven.co.kr')
-            req.add_header('Pragma', 'no-cache')
-            req.add_header('Referer', 'http://imart.inven.co.kr/attendance/')
-            req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.104 Safari/537.36')
-            req.add_header('X-Requested-With', 'XMLHttpRequest')
-        print req.headers
-        res = opener.open(req)
-        return BeautifulSoup(res.read(), 'html.parser')
+        request = urllib2.Request(url, params, headers)
+        print request.headers
+        response = self.opener.open(request)
+        return response.read()
 
-req = Requestor();
-stoken = req.get(URL + '/user/scorpio/mlogin', {}, False).find(id='stoken')['value']
+    def get_bs(self, url, params='', headers={}):
+        response = self.get_plain(url, params, headers)
+        return BeautifulSoup(response, 'html.parser')
+
+    def get_json(self, url, params='', headers={}):
+        response = self.get_plain(url, params, headers)
+        return json.loads(response)
+
+req = Request()
+STOKEN = req.get_bs(URL + '/user/scorpio/mlogin').find(id='stoken')['value']
 
 params = urllib.urlencode({
-    'st': stoken,
+    'st': STOKEN,
     'kp': 0,
     'user_id': ID,
     'password': PASSWORD
 })
-print req.get(URL + '/m/login/dispatch', params, False)
+print req.get_json(URL + '/m/login/dispatch', params)
 params = urllib.urlencode({
     'mid': ID
 })
-print req.get(URL + '/user-scorpio/extend_pw_result.php', params, False)
-print req.get(URL + '/user/scorpio/chk/skill/point', {}, False)
+print req.get_json(URL + '/user-scorpio/extend_pw_result.php', params)
+print req.get_json(URL + '/user/scorpio/chk/skill/point')
 
-attendCode = strftime('%Y%m',localtime())
+# TODO login error
+attendCode = strftime('%Y%m', localtime())
 params = urllib.urlencode({'attendCode': attendCode})
-print req.get('http://imart.inven.co.kr/attendance/attend_apply.ajax.php', params, True)
+headers = {
+    'Accept': '*/*',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'ko-KR,ko;q=0.8,en-US;q=0.6,en;q=0.4',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive',
+    'Content-Length': '17',
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'Host': 'imart.inven.co.kr',
+    'Origin': 'http://imart.inven.co.kr',
+    'Pragma': 'no-cache',
+    'Referer': 'http://imart.inven.co.kr/attendance/',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \
+                (KHTML, like Gecko) Chrome/59.0.3071.104 Safari/537.36',
+    'X-Requested-With': 'XMLHttpRequest',
+}
+print req.get_plain('http://imart.inven.co.kr/attendance/attend_apply.ajax.php', params, headers)
 
-params = urllib.urlencode({
-    'vidx': 3480,
-    'rurl': 'close',
-    'answer[]': 24,
-    'answer[]': 28,
-    'content': ''
-})
-# req.get('http://www.inven.co.kr/common/invenvote/vote_entry.php', params, False)
+# TODO GET vidx
+# VOTE_URL = req.get_bs('http://imart.inven.co.kr/attendance/').find(class_='voteBttn').contents[1]['href']
+# print 'VOTE_URL: ' + VOTE_URL
+# print req.get_bs(VOTE_URL).find_all('iframe', name='vidx')
+# # print 'VIDX: ' + VIDX
+# params = urllib.urlencode({
+#     'vidx': 3504,
+#     'rurl': 'close',
+#     'answer[]': 24,
+#     'answer[]': 28,
+#     'content': ''
+# })
+# print req.get('http://www.inven.co.kr/common/invenvote/vote_entry.php', params, False)
